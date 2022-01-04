9ad2aa97d26cf958fb8a4979085db8bd3e0adc34
Kajetan Maj <kajetan.maj@capgemini.com>
2022-01-04 15:10:09 +0100

    Added coloring when unpushed changes are detected in repository

--- auto_update.sh
+++ auto_update.sh
@@ -17,18 +17,19 @@ LAST_COMMIT=$(git show --no-notes --format=format:"%H" origin/$BRANCH | head -n 
 
 # IF we don't match we should update local
 if [ "$LAST_COMMIT" != "$LAST_UPDATE" ]; then
-        echo "Updating $DOTFILES from branch $BRANCH"
-        git pull --no-edit
-        $DOTFILES/install
-        omz reload
+  echo "Updating $DOTFILES from branch $BRANCH"
+  git pull --no-edit
+  $DOTFILES/install
+  omz reload
 else
-        echo "No updates in $DOTFILES available"
+  echo "No updates in $DOTFILES available"
 fi
 
 if [[ `git status --porcelain` ]]; then
-	echo "There are uncommited changes in $DOTFILES!"
-    echo "Commit and push changes."
-    exit
+  # tput explained in: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+  echo "$(tput setaf 1)There are uncommited changes in $DOTFILES!"
+  echo "$(tput setaf 1)$(tput smso)Commit and push changes."
+  exit
 fi
 
 cd "$START_DIR" || exit
