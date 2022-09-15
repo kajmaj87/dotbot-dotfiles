0d5af7598ac98c8996b94aa0c7e96f731b1c1567
Kajetan Maj <kajetan.maj@capgemini.com>
2022-09-15 16:26:25 +0200

    Fixed luxmed script

--- scripts/luxmed.sh
+++ scripts/luxmed.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 if [[ $# -eq 0 ]] ; then
-    echo "To use this script copy request as curl from browser and replace curl with $0 in command line"
+    echo "To use this script copy request as curl from browser and paste without changes after $0 in command line"
     echo "You also need to provide PUSHOVER_TOKEN and PUSHOVER_USER environment variables to for this to work"
     exit 0
 fi
 
+# remove "curl" from pasted curl 
+shift
+response=0
+while [ $response -eq 0 ]
+do
+
 raw_response=$(curl --compressed -s "$@")
-response=$(echo "$raw_response" | jq '[.termsForService.termsInfoForDays[].termsCounter.termsNumber] | add')
+response=$(echo "$raw_response" | jq '[.termsForService.termsInfoForDays[] | select(.message|contains("W tym dniu dostępne są terminy w innych")|not) | .termsCounter.termsNumber] | add')
 
 echo "$raw_response"
 echo "Total visits: $response"
@@ -16,3 +22,8 @@ echo "Total visits: $response"
   --form-string "user=$PUSHOVER_USER" \
   --form-string "message=Wizyta jest dostepna!" \
   https://api.pushover.net/1/messages.json
+
+
+sleep 15
+
+done
